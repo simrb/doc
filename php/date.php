206751cc29b9c3c0fc00cc8a4247b20409b05063
zcdny <151210739@qq.com>
2018-01-07 15:15:39 +0800

    update date demo

--- php/date.php
+++ php/date.php
@@ -3,15 +3,19 @@ date_default_timezone_set('Asia/Shanghai');
 
 
 $time = time();
-$date = date('YmdHis', $time);
+$mtime = microtime();
+$three_month_ago = $time-60*60*24*30*3;
 
 
-echo microtime();
+// echo $mtime;
 echo "\n";
-echo $time;
+// echo $time;
 echo "\n";
-echo $date;
-echo "\n";
-echo date('Y-m-d His', $date);
 
+echo date('Y-m-d H:i:s', $time);
+echo "\n";
+echo date('Y-m-d H:i:s', $three_month_ago);
+
+
+echo "\n";
 ?>
